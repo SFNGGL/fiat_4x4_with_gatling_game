260aa238dbabeff3bf775503321b615929dc5167
SFNGGL <stefanogigligigli@gmail.com>
2023-12-20 10:05:04 +0100

    General improvement to Forces calculations

--- car.js
+++ car.js
@@ -1,95 +1,90 @@
-function Car(x, y) { 
+function Car(x, y) {
   // CONSTANT: Weight, length...
   this.force = 20.0;
   this.L = 21.7;
   this.H = 12.56;
   this.M = 740;
-  
-  this.pos = createVector(x, y) 
+
+  this.pos = createVector(x, y)
   this.v = createVector(0, 0)
   this.a = createVector(0, 0);
-  this.dir = p5.Vector.fromAngle(x, 1);
-  
+  this.dir = p5.Vector.fromAngle(0);
+
   this.speed = 0;
-  
+
   this.EngineForce = 1000;
   this.rpm = 1000;
-  
-  this.C_drag = 12.8;
-  this.C_roll = 5/30;
+
+  this.C_drag = 30;
+  this.C_roll = this.C_drag / 30;
   this.C_brake = 4.5;
 
-  this.F_traction = createVector(0, 0);
+  this.F_traction = this.dir.copy();
   this.F_drag = createVector(0, 0);
   this.F_roll = createVector(0, 0);
   this.F_long = createVector(0, 0);
-  
-  this.accelerate = function(){
-    if (!(this.rpm > 6100)){
-      this.rpm += dt*this.force * 5;
+  this.sum = this.F_long.copy();
+
+  this.accelerate = function() {
+    if (!(this.rpm > 6100)) {
+      this.rpm += dt * this.force * 5;
     }
     this.EngineForce = findTorque(this.rpm);
   }
-  
-  this.decelerate = function(){
+
+  this.decelerate = function() {
     if (!(this.rpm <= 1000)) {
-      this.rpm -= dt*this.force * 1/5;
+      this.rpm -= dt * this.force * 5;
     }
-    if (this.rpm < 1000){
+    if (this.rpm < 1000) {
       this.rpm = 1000
     }
     this.EngineForce = findTorque(this.rpm);
   }
-  
-  this.brake = function(){}
-  
-  this.r = function(){}
-  
-  this.add_acceleration = function(increment){
-    this.a.add(increment);
-    this.dir.add(increment);
-    //this.dir.mult
-  }
-  
-  //this.compute_a = function(){
-    //this.a = p5.Vector.div(this.F_long, this.M)
-  //}
-  
-  this.update = function(){
-    
-    this.F_traction = p5.Vector.mult(
-      this.dir, this.Engineforce)
+
+  this.brake = function() { }
+
+  this.r = function() { }
+
+  this.update = function() {
+
+    this.F_traction = p5.Vector.mult(this.dir, this.EngineForce);
     this.F_drag = p5.Vector.mult(
-      this.v, this.speed*(-this.C_drag))
+      this.dir, this.speed * (-this.C_drag))
     this.F_roll = p5.Vector.mult(
-      this.v, this.C_roll)
-    
-    this.F_long.add(this.F_traction)
-    this.F_long.add(this.F_drag)
-    this.F_long.add(this.F_roll)
-    
-    this.a = p5.Vector.mult(this.F_long, 1/this.M)
-    
+      this.dir, this.C_roll)
+
+    this.sum.add(this.F_traction.mult(100))
+    this.sum.add(this.F_drag)
+    this.sum.add(this.F_roll)
+
+    if (this.sum != this.F_long) {
+      this.F_long = this.sum;
+      this.sum = createVector(0, 0);
+    }
+
+    this.a = p5.Vector.mult(this.F_long, 1 / this.M)
+
     this.v.add(p5.Vector.mult(this.a, dt))
     this.pos.add(p5.Vector.mult(this.v, dt))
-    
+
     if (this.pos.x <= 0) {
       this.pos.x += width
     }
-    
-    this.speed = this.v.magSq()
-    
-    //this.compute_a();
-    
+
+    this.speed = Math.sqrt(this.v.magSq());
+
+
   }
-  
-  this.reset = function(){
-    this.pos = createVector(x, y); 
+
+  this.reset = function() {
+    this.rpm = 1000;
+    this.pos = createVector(x, y);
     this.v = createVector(0, 0);
     this.a = createVector(0, 0);
-    this.dir = p5.Vector.fromAngle(0, this.a.mag());
+    this.dir = p5.Vector.fromAngle(0);
     this.F_long = createVector(0, 0);
   }
-  
-  
+
+
 }
--- sketch.js
+++ sketch.js
@@ -1,58 +1,74 @@
 const FPS = 60;
 const DIR_LENGTH = 20;
 
-var dt= 1/FPS;
+var dt = 1 / FPS;
 var car;
+var pause = false;
 
 function setup() {
   frameRate(FPS);
   createCanvas(500, 500);
-  
-  car = new Car(width/2, height/2);
+
+  car = new Car(width / 2, height / 2);
   car.reset();
 }
 
 function draw() {
   fill('white');
-  
+
   background(200);
-  
+
   push();
-  
-  text("Acceleration:"+ car.a.toString().split(':')[1], 6, 12);
-  text("Velocity:"+ car.v.toString().split(':')[1], 6, 24);
-  text("Position: " + car.pos.toString().split(':')[1], 6, 36);
-  text("Speed:"+ car.speed, 6, 48)
-  text("RPM:"+ int(car.rpm) +" | Engine force:"+ car.EngineForce, 6, 60)
-  text("Longitudinal traction: "+ car.F_traction.toString().split(':')[1], 6, 72)
-  text("Dragging Resistence: "+ car.F_drag.mag(), 6, 84)
-  text("Rolling Resistence: "+ car.F_roll.mag(), 6, 96)
-  text("Sum of forces: "+ car.F_long.mag(), 6, 108)
-  
+
+  info();
+  isPaused();
+
   car.pos.rem(width, height)
-    
+
   rectMode(CENTER)
   rect(car.pos.x, car.pos.y, car.L, car.H);
-  
-  drawArrow(car.pos, p5.Vector.add(car.dir, sign(car.dir) * (car.H) + DIR_LENGTH), 'black');
-  
+
+  drawArrow(car.pos, p5.Vector.add(car.dir, sign(car.dir.x) * (car.L) + car.dir.mag()), 'black');
+
   if (!keyIsDown(UP_ARROW)) {
     car.decelerate() // dare gas
   } else {
     car.accelerate() // "frenare"
   }
-  
+
   car.update();
-  
+
   pop();
-  
+
 }
 
-function sign(vec){
-  if (Math.sign(vec) > 0) {
+function info() {
+  let start_pad = 12;
+  let pad = 6;
+  text("Acceleration:" + car.a.toString().split(':')[1], start_pad, start_pad);
+  text("Velocity:" + car.v.toString().split(':')[1], start_pad, start_pad + (pad * 2));
+  text("Position: " + car.pos.toString().split(':')[1], start_pad, start_pad + (pad * 4));
+  text("Speed:" + car.speed, start_pad, start_pad + (pad * 6))
+  text("RPM:" + int(car.rpm) + " | Engine force:" + car.EngineForce, start_pad, start_pad + (pad * 8))
+  text("Longitudinal traction: " + car.F_traction.mag(), start_pad, start_pad + (pad * 10))
+  text("Dragging Resistence: " + car.F_drag.mag(), start_pad, start_pad + (pad * 12))
+  text("Rolling Resistence: " + car.F_roll.mag(), start_pad, start_pad + (pad * 14))
+  text("Sum of forces: " + car.F_long.mag(), start_pad, start_pad + (pad * 16))
+}
+
+function sign(vec) {
+  if (Math.sign(vec) >= 0) {
     return 1
   }
   return -1
+}
+
+function isPaused() {
+  if (pause) {
+    dt = 0;
+  } else {
+    dt = 1 / FPS;
+  }
 }
 
 function drawArrow(base, vec, myColor) {
@@ -66,13 +82,13 @@ function drawArrow(base, vec, myColor) {
   if (vec.mag != 0) {
     let arrowSize = 7;
     translate(vec.mag() - arrowSize, 0);
-    triangle(0 + DIR_LENGTH, arrowSize / 2, 0 + DIR_LENGTH, -arrowSize / 2, arrowSize + DIR_LENGTH, 0);
+    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
   }
   pop();
-  
+
 }
 
-function move(){
+function move() {
   if (keyIsDown(UP_ARROW)) {
     car.add_acceleration(createVector(dt, 0)) // dare gas
   } else if (keyIsDown(DOWN_ARROW)) {
@@ -80,7 +96,7 @@ function move(){
   }
 }
 
-function keyPressed(){
+function keyPressed() {
   if (keyCode === UP_ARROW) {
     //accelerare
     //car.add_acceleration(createVector(1*dt, 0));
@@ -93,5 +109,7 @@ function keyPressed(){
     //girare "a Sx" : (CCW)
   } else if (keyCode === 82) {
     car.reset();
+  } else if (keyCode === 80) {
+    pause = !pause;
   }
 }
