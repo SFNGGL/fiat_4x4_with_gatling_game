17a811ec00169c41122b7d79092e6dc6d2cb9782
SFNGGL <stefanogigligigli@gmail.com>
2023-12-27 16:07:07 +0100

    Revert "Push per aggiornate (tipo almeno 10 commit pushati non alla directory di github -_-)"
    This reverts commit 87dadbf48a8a8f73ecbe4f0bf2793213cbe72f54.

--- sketch.js
+++ sketch.js
@@ -5,85 +5,35 @@ var dt = 1 / FPS;
 var car;
 var pause = false;
 
-var colors = {
-  'dir': 'black',
-  'a': 'lightblue',
-  'v': 'orange',
-  'd1': 'green',
-  'd2': 'red',
-};
-
-let font;
-let cam;
-
-function preload() {
-  font = loadFont('./fonts/3270NerdFont-Regular.ttf');
-}
-
 function setup() {
   frameRate(FPS);
-  rectMode(CENTER);
-  angleMode(RADIANS);
-  createCanvas(1000, 800, WEBGL);
+  createCanvas(500, 500);
 
-  car = new Car(0, 0);
+  car = new Car(width / 2, height / 2);
   car.reset();
-
-  textFont(font);
-  textSize(16);
 }
 
 function draw() {
-
-  background(80);
-
   fill('white');
 
+  background(200);
+
   push();
 
-  // info();
+  info();
   isPaused();
 
-  rectMode(CENTER);
+  car.pos.rem(width, height)
 
-  push();
-
-  translate(car.pos.x, car.pos.y);
-  rotate(PI - car.angle);
-  translate(-car.pos.x, -car.pos.y);
+  rectMode(CENTER)
   rect(car.pos.x, car.pos.y, car.L, car.H);
 
-  pop();
+  drawArrow(car.pos, p5.Vector.add(car.dir, sign(car.dir.x) * (car.L) + car.dir.mag()), 'black');
 
-  push();
-
-  // drawArrow(car.pos, car.normal, colors['dir'], 2, car.L);
-  // drawArrow(car.pos, car.dir, colors['dir'], 4, car.L);
-  // drawArrow(car.pos, createVector(car.dir.x, 0), colors['d1'], 4, car.L);
-  // drawArrow(car.pos, createVector(0, car.dir.y), colors['d1'], 4, car.L);
-  // drawArrow(car.pos, createVector(car.v_vector.x, 0), colors['v'], 2, car.L * 1.5);
-  // drawArrow(car.pos, createVector(0, car.v_vector.y), colors['v'], 2, car.L * 1.5);
-  // drawArrow(car.pos, createVector(car.a_vector.x, 0), colors['a'], 2, car.L * 2);
-  // drawArrow(car.pos, createVector(0, car.a_vector.y), colors['a'], 2, car.L * 2);
-
-  pop();
-
-  if (keyIsDown(UP_ARROW)) {
-    car.accelerate(); // dare gas
-  } else if (keyIsDown(DOWN_ARROW)) {
-    if (car.v_scalar > 0) {
-      car.brake();
-    } else {
-      car.r();
-    }
+  if (!keyIsDown(UP_ARROW)) {
+    car.decelerate() // dare gas
   } else {
-    car.decelerate();
-  }
-
-  if (keyIsDown(LEFT_ARROW)) {
-    car.turn_left();
-  } else if (keyIsDown(RIGHT_ARROW)) {
-    car.turn_right();
+    car.accelerate() // "frenare"
   }
 
   car.update();
@@ -92,48 +42,25 @@ function draw() {
 
 }
 
-function drawArrow(base, vec, myColor, weight, scale) {
-  let arrowSize = 7;
-  push();
-  stroke(myColor);
-  strokeWeight(weight);
-  fill(myColor);
-  translate(base.x, base.y);
-  // if (base.dist(vec) <= arrowSize) { return; }
-  line(0, 0, vec.x * scale, vec.y * scale);
-  rotate(vec.heading());
-  if (vec.mag != 0) {
-    translate(vec.mag() * scale - arrowSize, 0);
-    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  }
-  pop();
+function info() {
+  let start_pad = 12;
+  let pad = 6;
+  text("Acceleration:" + car.a.toString().split(':')[1], start_pad, start_pad);
+  text("Velocity:" + car.v.toString().split(':')[1], start_pad, start_pad + (pad * 2));
+  text("Position: " + car.pos.toString().split(':')[1], start_pad, start_pad + (pad * 4));
+  text("Speed:" + car.speed, start_pad, start_pad + (pad * 6))
+  text("RPM:" + int(car.rpm) + " | Engine force:" + car.EngineForce, start_pad, start_pad + (pad * 8))
+  text("Longitudinal traction: " + car.F_traction.mag(), start_pad, start_pad + (pad * 10))
+  text("Dragging Resistence: " + car.F_drag.mag(), start_pad, start_pad + (pad * 12))
+  text("Rolling Resistence: " + car.F_roll.mag(), start_pad, start_pad + (pad * 14))
+  text("Sum of forces: " + car.F_long.mag(), start_pad, start_pad + (pad * 16))
 }
 
-function info() {
-  textSize(16);
-  let pad = 8;
-  let start_pad = -height / 2 + pad * 2;
-  fill(colors['a'])
-  text("Acceleration (X-axis): " + car.a_vector.x + "\t(Y-axis): " + car.a_vector.y,
-    -width / 2 + pad, -height / 2 + pad * 2);
-  fill(colors['v'])
-  text("Velocity (X-axis): " + car.v_vector.x + "\t(Y-axis): " + car.v_vector.y,
-    -width / 2 + pad, -height / 2 + pad * 4);
-  fill('white')
-  text("Position: " + car.pos.toString().split(':')[1],
-    -width / 2 + pad, -height / 2 + pad * 6);
-  text("Speed:" + car.speed,
-    -width / 2 + pad, -height / 2 + pad * 8);
-  fill(colors['d1'])
-  text("Angle: " + car.dir.heading() + " | " + car.a_wheel + " | " + car.a_tot,
-    -width / 2 + pad, -height / 2 + pad * 10);
-  fill('white')
-  if (pause) {
-    textSize(72);
-    fill('red');
-    text("PAUSE", -72 - 20, -72 / 2);
-    fill('white')
+function sign(vec) {
+  if (Math.sign(vec) >= 0) {
+    return 1
   }
+  return -1
 }
 
 function isPaused() {
@@ -144,8 +71,43 @@ function isPaused() {
   }
 }
 
+function drawArrow(base, vec, myColor) {
+  push();
+  stroke(myColor);
+  strokeWeight(3);
+  fill(myColor);
+  translate(base.x, base.y);
+  line(0, 0, vec.x, vec.y);
+  rotate(vec.heading());
+  if (vec.mag != 0) {
+    let arrowSize = 7;
+    translate(vec.mag() - arrowSize, 0);
+    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  }
+  pop();
+
+}
+
+function move() {
+  if (keyIsDown(UP_ARROW)) {
+    car.add_acceleration(createVector(dt, 0)) // dare gas
+  } else if (keyIsDown(DOWN_ARROW)) {
+    //car.add_acceleration(createVector(-dt, 0)) // "frenare"
+  }
+}
+
 function keyPressed() {
-  if (keyCode === 82) {
+  if (keyCode === UP_ARROW) {
+    //accelerare
+    //car.add_acceleration(createVector(1*dt, 0));
+  } else if (keyCode === DOWN_ARROW) {
+    //decelerare
+    //car.add_acceleration(createVector(-1, 0));
+  } else if (keyCode === RIGHT_ARROW) {
+    //girare "a Dx": (Clockwise)
+  } else if (keyCode === LEFT_ARROW) {
+    //girare "a Sx" : (CCW)
+  } else if (keyCode === 82) {
     car.reset();
   } else if (keyCode === 80) {
     pause = !pause;
